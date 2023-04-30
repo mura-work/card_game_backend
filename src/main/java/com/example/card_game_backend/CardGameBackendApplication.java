8664286c641664a79fb1b80967fbd009a4d52369
mura-work <murata.naoya.work@gmail.com>
2023-04-30 11:48:37 +0900

    first commit

---
+++ src/main/java/com/example/card_game_backend/CardGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.card_game_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardGameBackendApplication.class, args);
+	}
+
+}
