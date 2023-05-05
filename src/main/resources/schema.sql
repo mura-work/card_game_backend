643eaef1c21b52755b6e7c797b84e5e7735b3651
mura-work <murata.naoya.work@gmail.com>
2023-05-05 11:31:05 +0900

    [feat] lombokの導入とSQLの修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,33 @@
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS black_jack_histories;
+DROP TABLE IF EXISTS player_black_jack_hands;
+DROP TABLE IF EXISTS dealer_black_jack_hands;
 
-CREATE TABLE test (
+CREATE TABLE black_jack_histories (
     id INT PRIMARY KEY AUTO_INCREMENT,
     result VARCHAR(255) NOT NULL,
-    player_hands JSON,
-    dealer_hands JSON,
     playing_date_time DATE NOT NULL,
     user_id INT NOT NULL
 );
 
-INSERT INTO test (result, player_hands, dealer_hands, playing_date_time, user_id)
-VALUES ('WIN', null, null, '2023-04-01', 1000);
+CREATE TABLE player_black_jack_hands (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    black_jack_history_id INT NOT NULL,
+    suit VARCHAR(255) NOT NULL,
+    rank VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE dealer_black_jack_hands (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    black_jack_history_id INT NOT NULL,
+    suit VARCHAR(255) NOT NULL,
+    rank VARCHAR(255) NOT NULL
+);
+
+INSERT INTO black_jack_histories (result, playing_date_time, user_id)
+VALUES ('WIN', '2023-04-01', 1000);
+
+INSERT INTO player_black_jack_hands (black_jack_history_id, suit, rank)
+VALUES (1, 'CLUB', '3');
+
+INSERT INTO dealer_black_jack_hands (black_jack_history_id, suit, rank)
+VALUES (1, 'CLUB', 'A');
