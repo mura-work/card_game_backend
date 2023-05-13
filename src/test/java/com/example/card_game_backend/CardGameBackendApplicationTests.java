559d2d2d2e02ae40f856a0098517a9a9e5f25990
mura-work <murata.naoya.work@gmail.com>
2023-05-13 12:03:11 +0900

    [fix] ビルドエラーの修正

--- src/test/java/com/example/card_game_backend/CardGameBackendApplicationTests.java
+++ src/test/java/com/example/card_game_backend/CardGameBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.card_game_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {CardGameBackendApplication.class})
 class CardGameBackendApplicationTests {
 
 	@Test
